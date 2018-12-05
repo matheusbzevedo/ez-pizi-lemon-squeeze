5ade0ca75a48498932264b2119cf2b2b1e4e272d
Cleóbulo Oliveira <cleobulo.oliveira@gmail.com>
2018-12-05 10:25:07 -0400

    feat: Apagar e editar perfil

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -9,3 +9,26 @@ $('#add-perfil').on('click', (event) => {
     .then(response => { Materialize.toast(response.data, 2000, 'rounded', () => { location.reload(); }); })
     .catch(error => { Materialize.toast(error, 2000, 'rounded'); });
 });
+$('.edit-item').on('click', (event) => {
+    $('#lista-editar-item').ready(() => {
+        $('.input-field input[name=descricao]').attr('value', $(event.currentTarget).parent().prev().text());
+        $('.input-field input[name=id]').attr('value', $(event.currentTarget).parent().prev().prev().text());
+    })
+});
+$('#updt-perfil').on('click', (event) => {
+    $('.edit-item').ready(() => {
+        axios({
+            method: 'PUT',
+            url: '/api/perfil',
+            data: {
+                id: $('#id').val(),
+                descricao: $('#descricao').val()
+            }
+        })
+        .then(response => { Materialize.toast(response.data, 2000, 'rounded', () => { location.reload(); }); })
+        .catch(error => { Materialize.toast(error, 2000, 'rounded'); });
+    });
+});
+$('.del-item').on('click', (event) => {
+    return confirm('Deseja realmente apagar?');
+})
